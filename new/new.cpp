f01c08f37893616fc0746913cc81c28c41c6d17d
ShreyaKurry13 <shreyakurry13@gmail.com>
2025-02-27 21:34:12 +0530

    first c++ program

---
+++ new/new.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    float num1,num2;
+    cout<<"Enter the 2 numbers:";
+    cin>>num1>>num2;
+    cout<<num1<<"+"<<num2<<"="<<(num1+num2);
+
+    return 0;
+}
