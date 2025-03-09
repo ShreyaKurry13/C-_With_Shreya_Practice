06ca890601c358bb3eac2aa6b2373cb74b71572a
ShreyaKurry13 <shreyakurry13@gmail.com>
2025-03-09 16:39:35 +0530

    loops updated

--- new/loops2.cpp
+++ new/loops2.cpp
@@ -18,28 +18,49 @@ int main(){
     
     //2 LOOPS
 
-    int i,j;
-    // cout<<"Enter a number:";
-    // cin>>num;
+ /*   int i,j;
+    cout<<"Enter a number: ";
+    cin>>n;
 
-    for(i=1,j=9; i<=9,j>=1;i++,j--)
+    for (i = 1,j=9; i < 10,j>0; i++,j--)
     {
-       cout<<i<<"+"<<j<<"="<<(i+j)<<"\n";
+        cout<<i<<"+"<<j<<"="<<(i+j)<<"\n";
     }
+   */
 
     //o/p
-// 1+9=10
-// 2+8=10
-// 3+7=10
-// 4+6=10
-// 5+5=10
-// 6+4=10
-// 7+3=10
-// 8+2=10
-// 9+1=10
+    // 1+9=10
+    // 2+8=10
+    // 3+7=10
+    // 4+6=10
+    // 5+5=10
+    // 6+4=10
+    // 7+3=10
+    // 8+2=10
+    // 9+1=10
 
-     
+    // tables ka code
+ /*   int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int result;
 
+    for(int i=1;i<=10;i++){
+        result = n*i;
+        cout<<n<<" * "<<i<<" = "<<result<<"\n";
+    }
+   */
+  
+   
+   //two for loops
+   int i,j;
+
+   for(i=1;i<=3;i++){
+    for ( j = 1; j<=3; j++)
+    {
+        cout<<"\n i = "<<i<<" j = "<<j;
+    }
+   }
 
     return 0;
 }
